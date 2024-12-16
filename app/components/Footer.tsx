d9bfa066b5ac22393c8d49e9107255a57d55f8dd
chiragb <chiragb@hashtagloyalty.com>
2024-12-17 00:16:11 +0530

    fix footer text alingment

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
 
   return (
     <div className="flex flex-col items-center gap-2">
-      <div className="text-gray-500 font-semibold">
+      <div className="text-gray-500 font-semibold text-center">
         Built with Next.js, TypeScript, Tailwind & Deployed on GitHub Pages 🚀
       </div>
       <div className="text-gray-500 text-sm font-medium">
