8cd126c9e1e8b24c2b4b27da27fbef7bb682a77e
chiragb <chiragb@hashtagloyalty.com>
2024-12-19 17:32:19 +0530

    Update tag radius for smaller devices

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -21,20 +21,18 @@ type ProjectsProps = {
 export default function Projects({ projects }: Readonly<ProjectsProps>) {
   return (
     <Section title="Projects">
-      <div className="gap-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-        {projects.map((project) => (
-          <Card key={project.name} href={project.url}>
-            <div className="flex flex-col gap-1.5 md:gap-4 md:w-9/12">
-              <h4 className="group-hover/card:text-green-500 font-medium text-gray-300 text-lg">
-                {project.name}
-              </h4>
-              <SectionBody text={project.description} />
-              {project.appLinks && <AppLinks appLinks={project.appLinks} />}
-              <Tags titles={project.skillsUsed} />
-            </div>
-          </Card>
-        ))}
-      </div>
+      {projects.map((project) => (
+        <Card key={project.name} href={project.url}>
+          <div className="flex flex-col gap-1.5 md:gap-4 md:w-9/12">
+            <h4 className="group-hover/card:text-green-500 font-medium text-gray-300 text-lg">
+              {project.name}
+            </h4>
+            <SectionBody text={project.description} />
+            {project.appLinks && <AppLinks appLinks={project.appLinks} />}
+            <Tags titles={project.skillsUsed} />
+          </div>
+        </Card>
+      ))}
     </Section>
   );
 }
--- app/components/common/Tags.tsx
+++ app/components/common/Tags.tsx
@@ -2,7 +2,10 @@ export default function Tags({ titles }: Readonly<{ titles: string[] }>) {
   return (
     <div className="flex flex-wrap gap-1.5 md:gap-2">
       {titles.map((title) => (
-        <div key={title} className="bg-white/10 px-2 py-0 md:py-1 rounded-md">
+        <div
+          key={title}
+          className="bg-white/10 px-2 py-0 md:py-1 rounded-sm md:rounded-md"
+        >
           <span className="group-hover/card:text-gray-100 font-medium text-center text-gray-400 text-sm">
             {title}
           </span>
